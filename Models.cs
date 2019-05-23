63c12f65711f3c0f9641c42a1688ac8caeaca6d7
mfelix366 <mfelix366@gmail.com>
2019-05-23 22:27:59 +0530

    Completed BackEnd

--- Models.cs
+++ Models.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 
 namespace greenstar_api {
     public class SchoolNamesList {
@@ -6,5 +7,38 @@ namespace greenstar_api {
         public string address {get; set;}
         public string city {get; set;}
         public string country {get; set;}
+    }
+
+    public class Standard {
+        public string schoolId {get; set;}
+        public List<string> standardList = new List<string>();
+    }
+
+    public class SectionListForSchoolAndStandard {
+        public string schoolId {get; set;}
+        public string standard {get; set;}
+        public List<string> sectionList = new List<string>();
+    }
+
+    public class GroupListForSchoolAndStandardAndGroup {
+        public string schoolId {get; set;}
+        public string standard {get; set;}
+        public string section {get; set;}
+        public List<string> IGroupList = new List<string>();
+    }
+
+    public class StudentListForSchoolAndStandardAndSectionAndGroup {
+        public string schoolId {get; set;}
+        public string standard {get; set;}
+        public string section {get; set;}
+        public string group {get; set;}
+        public List<string> studentId = new List<string>();
+        public List<string> studentList = new List<string>();
+    }
+
+    public class ParameterNameParameterValue {
+        public string studentId {get; set;}
+        public List<string> parameterName = new List<string>();
+        public List<string> parameterValue = new List<string>();
     }
 }
--- school_param.cs
+++ school_param.cs
@@ -48,41 +48,156 @@ namespace greenstar_api
         [FunctionName("StandardsListForAGivenSchool")]
         public static async Task<IActionResult> StandardsListForAGivenSchool(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "standards/{id}")] HttpRequest req,
-            ILogger log) {
+            ILogger log,
+            string Id) {
+                
                 var dbConnect = Environment.GetEnvironmentVariable("AZURE_SQLSERVER_GREENSTARDB_CONNECTION");
-                return (ActionResult)new OkObjectResult("hello func");
+                
+                DataSet ds = new DataSet();
+                var standardList = new Standard();
+
+                using(SqlConnection conn = new SqlConnection(dbConnect)) {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("select distinct standard from dbo.greenstar_students where school_id ='" + Id.ToString() + "'", conn);
+                    using(SqlDataReader sdr = await cmd.ExecuteReaderAsync()) {
+                        ds.Tables.Add().Load(sdr);
+                    }
+                    standardList.schoolId = Id.ToString();
+                    foreach(DataRow row in ds.Tables[0].Rows) {
+                        standardList.standardList.Add(row[0].ToString());
+                    }
+                }
+                
+                return (ActionResult)new OkObjectResult(standardList);
         }
 
         [FunctionName("SectionsListForAGivenSchoolAndStandard")]
         public static async Task<IActionResult> SectionsListForAGivenSchoolAndStandard(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "sections/{id}")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "sections")] HttpRequest req,
             ILogger log) {
+
+                var schoolId = req.Query["school_id"];
+                var standardName = req.Query["standard"];
+
+                DataSet ds = new DataSet();
                 var dbConnect = Environment.GetEnvironmentVariable("AZURE_SQLSERVER_GREENSTARDB_CONNECTION");
-                return (ActionResult)new OkObjectResult("hello func");
+                var sectionListForSchoolAndStandard = new SectionListForSchoolAndStandard();
+
+                using(SqlConnection conn = new SqlConnection(dbConnect)) {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(
+                        "select distinct section from dbo.greenstar_students where school_id = '" + schoolId.ToString() + "' and standard = '" + standardName.ToString() + "'", 
+                        conn);
+                    using(SqlDataReader sdr = await cmd.ExecuteReaderAsync()) {
+                        ds.Tables.Add().Load(sdr);
+                    }
+                }
+
+                sectionListForSchoolAndStandard.schoolId = schoolId;
+                sectionListForSchoolAndStandard.standard = standardName;
+                foreach(DataRow row in ds.Tables[0].Rows) {
+                    sectionListForSchoolAndStandard.sectionList.Add(row[0].ToString());
+                }
+
+                return (ActionResult)new OkObjectResult(sectionListForSchoolAndStandard);
         }
 
         [FunctionName("GroupsListForAGivenSchoolAndStandardAndSection")]
         public static async Task<IActionResult> GroupsListForAGivenSchoolAndStandardAndSection(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "groups/{id}")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "groups")] HttpRequest req,
             ILogger log) {
+                var school_id = req.Query["school_id"];
+                var standardName = req.Query["standard"];
+                var sectionName = req.Query["section"];
+
+                DataSet ds = new DataSet();
                 var dbConnect = Environment.GetEnvironmentVariable("AZURE_SQLSERVER_GREENSTARDB_CONNECTION");
-                return (ActionResult)new OkObjectResult("hello func");
+                var groupsListForAGivenSchoolAndStandardAndSection = new GroupListForSchoolAndStandardAndGroup();
+
+                using(SqlConnection conn = new SqlConnection(dbConnect)) {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(
+                        "select distinct group_name from dbo.greenstar_students where school_id = '" + school_id.ToString() + "' and standard = '" + standardName.ToString() + "' and section = '" + sectionName.ToString() + "'", 
+                        conn);
+                    using(SqlDataReader sdr = await cmd.ExecuteReaderAsync()) {
+                        ds.Tables.Add().Load(sdr);
+                    }
+                }
+                
+                groupsListForAGivenSchoolAndStandardAndSection.schoolId = school_id.ToString();
+                groupsListForAGivenSchoolAndStandardAndSection.standard = standardName.ToString();
+                groupsListForAGivenSchoolAndStandardAndSection.section = sectionName.ToString();
+                foreach(DataRow row in ds.Tables[0].Rows) {
+                    groupsListForAGivenSchoolAndStandardAndSection.IGroupList.Add(row[0].ToString());
+                }
+
+                return (ActionResult)new OkObjectResult(groupsListForAGivenSchoolAndStandardAndSection);
         }
 
         [FunctionName("StudentListForAGivenSchoolAndStandardAndSection")]
         public static async Task<IActionResult> StudentListForAGivenSchoolAndStandardAndSection(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "students")] HttpRequest req,
             ILogger log) {
+                var school_id = req.Query["school_id"];
+                var standardName = req.Query["standard"];
+                var sectionName = req.Query["section"];
+                var groupName = req.Query["group"];
+
+                DataSet ds = new DataSet();
                 var dbConnect = Environment.GetEnvironmentVariable("AZURE_SQLSERVER_GREENSTARDB_CONNECTION");
-                return (ActionResult)new OkObjectResult("hello func");
+                var studentListForAGivenSchoolAndStandardAndSection = new StudentListForSchoolAndStandardAndSectionAndGroup();
+
+                using(SqlConnection conn = new SqlConnection(dbConnect)) {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(
+                        "select distinct student_id,student_name from dbo.greenstar_students where school_id = '" + 
+                        school_id.ToString() + "' and standard = '" + standardName.ToString() + "' and section = '" + 
+                        sectionName.ToString() + "' and group_name = '" + groupName.ToString() + "'", 
+                        conn);
+                    using(SqlDataReader sdr = await cmd.ExecuteReaderAsync()) {
+                        ds.Tables.Add().Load(sdr);
+                    }
+                }
+
+                studentListForAGivenSchoolAndStandardAndSection.schoolId = school_id.ToString();
+                studentListForAGivenSchoolAndStandardAndSection.standard = standardName.ToString();
+                studentListForAGivenSchoolAndStandardAndSection.section = sectionName.ToString();
+                studentListForAGivenSchoolAndStandardAndSection.group = groupName.ToString();
+                foreach(DataRow row in ds.Tables[0].Rows) {
+                    studentListForAGivenSchoolAndStandardAndSection.studentId.Add(row[0].ToString());
+                    studentListForAGivenSchoolAndStandardAndSection.studentList.Add(row[1].ToString());
+                }
+
+                return (ActionResult)new OkObjectResult(studentListForAGivenSchoolAndStandardAndSection);
         }
 
         [FunctionName("ParameterListForAGivenSchoolAndStandardAndSectionAndStudentName")]
         public static async Task<IActionResult> ParameterListForAGivenSchoolAndStandardAndSectionAndStudentName(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "parameters")] HttpRequest req,
             ILogger log) {
+                var student_id = req.Query["student_id"];
+
+                DataSet ds = new DataSet();
                 var dbConnect = Environment.GetEnvironmentVariable("AZURE_SQLSERVER_GREENSTARDB_CONNECTION");
-                return (ActionResult)new OkObjectResult("hello func");
+                var parameterNameParameterValue = new ParameterNameParameterValue();
+
+                using(SqlConnection conn = new SqlConnection(dbConnect)) {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(
+                        "select paramter_name,parameter_value,Date from dbo.greenstar_parameters where student_id = '" + student_id.ToString() + "'", 
+                        conn);
+                    using(SqlDataReader sdr = await cmd.ExecuteReaderAsync()) {
+                        ds.Tables.Add().Load(sdr);
+                    }
+                }
+
+                parameterNameParameterValue.studentId = student_id.ToString();
+                foreach(DataRow row in ds.Tables[0].Rows) {
+                    parameterNameParameterValue.parameterName.Add(row[0].ToString());
+                    parameterNameParameterValue.parameterValue.Add(row[1].ToString());
+                }
+
+                return (ActionResult)new OkObjectResult(parameterNameParameterValue);
         }
     }
 }
